cf6faf1d3bf459b48917835085617b1a217b595d
pato <patricio.dfernandez@gmail.com>
2022-01-31 12:58:52 -0300

    install dependences, axios, redux logger,- configure logger y reduxstore

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../features/counter/counterSlice';
+import contactReducer from "./state/contact";
+
+import logger from "redux-logger";
 
 export default configureStore({
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   reducer: {
     counter: counterReducer,
+    contact: contactReducer,
   },
 });
