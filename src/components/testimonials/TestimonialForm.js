c4656a7c813dd607f1fab7faed45e8fe07215202
Iván Rodrigo Ferrioli <ivan.rf.96@gmail.com>
2022-02-07 17:21:11 -0300

    implements update and add handlers

--- src/components/testimonials/TestimonialForm.js
+++ src/components/testimonials/TestimonialForm.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 import {
@@ -6,71 +7,200 @@ import {
   HStack,
   VStack,
   Button,
-  Image,
   FormLabel,
   Input,
   Spacer,
+  FormControl,
+  Image,
+  Box,
 } from '@chakra-ui/react'
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-import TextField from '../TextField'
+import { add, getTestimonial, update } from '../../services/TestimonialsService'
+import Alert from '../alert/Alert'
 
-const TestimonialForm = () => (
-  <Formik
-    enableReinitialize
-    initialValues={{
-      name: '',
-      image: '',
-      content: '',
-    }}
-    validationSchema={Yup.object({
-      name: Yup.string()
-        .required('Nombre requerido!')
-        .min(3, 'Nombre muy corto!'),
-    })}
-    onSubmit={() => {}}
-  >
-    {(formik) => (
-      <HStack display="flex" height="100vh" backgroundColor="#FAFA88">
-        <VStack
-          as="form"
-          m="auto"
-          p="2"
-          w={{ base: '90%', md: 500 }}
-          h="auto"
-          justifyContent="center"
-          borderWidth="1px solid white"
-          borderRadius="lg"
-          boxShadow="lg"
-          backgroundColor="white"
-          display="block"
-          onSubmit={formik.handleSubmit}
+const TestimonialForm = () => {
+  const { id } = useParams()
+  const [testimonialData, setTestimonialData] = useState({
+    id: null,
+    name: '',
+    image:
+      'https://nypost.com/wp-content/uploads/sites/2/2021/12/nature_14.jpg?quality=80&strip=all&w=744',
+    content: '',
+  })
+  const [ready, setReady] = useState(false)
+  const [alerts, setAlerts] = useState({
+    show: false,
+    title: '',
+    message: '',
+    icon: '',
+    onConfirm: () => {},
+  })
+  const loadTestimonialData = async () => {
+    if (id) {
+      try {
+        const loadedTestimonialData = await getTestimonial(id)
+        setTestimonialData({
+          id: loadedTestimonialData.data.result.id,
+          name: loadedTestimonialData.data.result.name,
+          image:
+            'https://nypost.com/wp-content/uploads/sites/2/2021/12/nature_14.jpg?quality=80&strip=all&w=744',
+          content: loadedTestimonialData.data.result.content,
+        })
+        setReady(true)
+      } catch (error) {
+        const errorAlert = {
+          show: true,
+          title: 'Ooops, algo ha fallado!',
+          message: error.message,
+          icon: 'error',
+          onConfirm: () => {},
+        }
+        setAlerts(errorAlert)
+      }
+    }
+  }
+  useEffect(() => {
+    loadTestimonialData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const ckChangeHandler = (event, editor) => {
+    const editedData = editor.getData()
+    setTestimonialData((data) => ({ ...data, content: editedData }))
+  }
+  const updateChangeHandler = async (values) => {
+    const updatedTestimonial = await update(id, {
+      name: values.name,
+      content: testimonialData.content,
+      image: testimonialData.image,
+    })
+    if (updatedTestimonial) {
+      const successAlert = {
+        show: true,
+        title: 'Testimonio',
+        message: 'El testimonio se ha actualizado!',
+        icon: 'success',
+        onConfirm: () => {},
+      }
+      setAlerts(successAlert)
+    }
+  }
+
+  const AddSubmitHandler = async (values) => {
+    try {
+      const newTestimonial = await add({
+        name: values.name,
+        content: testimonialData.content,
+        image: values.image,
+      })
+      if (newTestimonial) {
+        const successAlert = {
+          show: true,
+          title: 'Testimonio',
+          message: 'Testimonio agregado!',
+          icon: 'success',
+          onConfirm: () => {},
+        }
+        setAlerts(successAlert)
+      }
+    } catch (error) {
+      const errorAlert = {
+        show: true,
+        title: 'hubo un error!',
+        message: error.message,
+        icon: 'error',
+        onConfirm: () => {},
+      }
+      setAlerts(errorAlert)
+    }
+  }
+
+  return (
+    <>
+      <Alert {...alerts} />
+      {((id && ready) || !id) && (
+        <Formik
+          initialValues={testimonialData}
+          validationSchema={Yup.object({
+            name: Yup.string()
+              .required('Nombre requerido!')
+              .min(3, 'Nombre muy corto!'),
+          })}
+          onSubmit={(values) =>
+            (id ? updateChangeHandler(values) : AddSubmitHandler(values))}
         >
-          <Heading align="center">Testimonio</Heading>
-          <TextField name="name" placeholder="Name" />
-          <Spacer />
-          <CKEditor name="content" editor={ClassicEditor} />
-          <FormLabel paddingLeft="2">Imagen</FormLabel>
-          <Input
-            id="filePicker"
-            width="230px"
-            display="block"
-            border-radius="5px"
-            padding="4px 5px"
-            cursor="pointer"
-            type="file"
-          />
-          <Image
-            src="https://nypost.com/wp-content/uploads/sites/2/2021/12/nature_14.jpg?quality=80&strip=all&w=744"
-            objectFit="cover"
-          />
-          <Button type="submit" w="100%">
-            Guardar
-          </Button>
-        </VStack>
-      </HStack>
-    )}
-  </Formik>
-)
+          {({ values, handleSubmit, handleChange }) => (
+            <HStack
+              display="flex"
+              height="100vh"
+              backgroundColor="#FAFA88"
+              width="100%"
+            >
+              <VStack
+                as="form"
+                m="auto"
+                p="2"
+                w={{ base: '90%', md: 400, sm: 300 }}
+                h="auto"
+                justifyContent="center"
+                borderWidth="1px solid white"
+                borderRadius="lg"
+                boxShadow="lg"
+                backgroundColor="white"
+                display="block"
+                onSubmit={handleSubmit}
+              >
+                <Heading align="center">Testimonio</Heading>
+                <FormControl>
+                  <FormLabel paddingLeft="2">Titulo</FormLabel>
+                  <Input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                  />
+                </FormControl>
+                <Spacer />
+                <FormControl>
+                  <FormLabel paddingLeft="2">Contenido</FormLabel>
+                  <CKEditor
+                    name="content"
+                    data={values.content}
+                    editor={ClassicEditor}
+                    onChange={ckChangeHandler}
+                  />
+                </FormControl>
+                <FormLabel paddingLeft="2">Imagen</FormLabel>
+                <FormControl
+                  display="flex"
+                  flexWrap="wrap"
+                  justifyContent="center"
+                >
+                  <Box paddingBottom="2" align="center">
+                    <Input
+                      width="220px"
+                      border-radius="5px"
+                      padding="4px 5px"
+                      cursor="pointer"
+                      type="file"
+                    />
+                  </Box>
+                  <Box paddingLeft="4" paddingRight="4">
+                    <Image objectFit="cover" src={testimonialData.image} />
+                  </Box>
+                </FormControl>
+                <Button type="submit" w="100%">
+                  Guardar
+                </Button>
+              </VStack>
+            </HStack>
+          )}
+        </Formik>
+      )}
+    </>
+  )
+}
 
 export default TestimonialForm
