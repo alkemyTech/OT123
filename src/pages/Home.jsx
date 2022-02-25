5a8b18fc47bd14326f2e455865b704906bcc085b
martinmolina1988 <67392381+martinmolina1988@users.noreply.github.com>
2022-02-25 15:06:15 -0300

    add welcometext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,32 @@ import {
 } from '@chakra-ui/react'
 import { getAllSliders } from '../services/slidersService'
 import Slider from '../components/home/Slider'
+import { getOrganizationById } from '../services/organizationsService'
 
 const Home = () => {
   const [sliderData, setSliderData] = useState([])
+  // eslint-disable-next-line no-unused-vars
+  const [organizationData, setOrganizationData] = useState({})
 
   const loadSliders = async () => {
    const sliders = await getAllSliders()
    setSliderData(sliders.data.result.sliders)
   }
+  const loadOrganization = async () => {
+   const data = await getOrganizationById(1)
+   console.table(data.data.result.publicData);
+   setOrganizationData(data.data.result.publicData)
+  }
 
   useEffect(() => {
     loadSliders();
+    loadOrganization()
   }, [])
 
   return (
     <>
       <Slider sliderData={sliderData} />
-      <Text textAlign="center" fontSize="3xl" my={5}>Texto de Bienvenida</Text>
+      <Text textAlign="center" fontSize="3xl" my={5}>{organizationData?.welcomeText}</Text>
       <Text textAlign="center" fontSize="2xl" mb={2}>Últimas novedades</Text>
       <Text textAlign="center" fontSize="2xl">Testimonios</Text>
     </>
